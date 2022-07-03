9af6ae768e0302a7203bd217d131a867350df39a
Pawan Paudel <pawanpaudel93@gmail.com>
2022-07-03 16:23:55 +0545

    removed unnecessary packages and updated code

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Flashbots-ens
-1) Run the following commands to install the dependencies:
+1. Run the following commands to install the dependencies:
 ```
 yarn
 ```
 
-2) Copy .env.example to .env and fill in the values.
+2. Copy .env.example to .env and fill in the values.
 
-3) Edit ensRegister.ts in the scripts folder to set the desired ens name.
+3. Edit ensRegister.ts in the scripts folder to set the desired ens name.
 
-4)Run the following commands to run the ensRegister script:
+4.  Run the following commands to run the ensRegister script:
 ```
 npx hardhat run scripts/ensRegister.ts --network goerli
 ```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,8 @@
 import * as dotenv from "dotenv";
 
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
 
 dotenv.config();
 
@@ -25,13 +22,6 @@ const config: HardhatUserConfig = {
       url: process.env.ALCHEMY_RPC_URL,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
